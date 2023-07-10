ca17df8a6b910ec80a1d82ccddf0756d13ff7ba3
rajalaxmi_khatai <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-07-10 22:31:15 +0530

    update why js used in mern

--- JavaScriptNotes/introduction.js
+++ JavaScriptNotes/introduction.js
@@ -11,3 +11,15 @@
 // Sure! Here's a basic example of JavaScript code that displays a message in the console:
 console.log("Hello, world!");  //o/p:-hello world
 // This code uses the console.log() function to print the message "Hello, world!" to the browser's console
+// lets get started why javascript is used in mern development?
+// JavaScript is a versatile programming language that can be used both on the front-end and back-end of web development. In MERN (MongoDB, Express.js, React, Node.js) stack, JavaScript is used to build different components of the application.
+
+//1. Front-end Development: JavaScript is primarily used for building interactive and dynamic user interfaces in MERN applications. React, a popular JavaScript library, is used for creating UI components and handling the presentation layer. It allows you to create reusable UI elements and efficiently manage the application state.
+
+// 2.Back-end Development: Node.js is a server-side JavaScript runtime environment, which enables JavaScript to be executed on the server. It provides an efficient and scalable platform for building the server-side logic of MERN applications. Node.js has a wide range of libraries and frameworks available for implementing server-side functionalities, such as Express.js, which is a lightweight web framework for Node.js. Express.js helps in handling HTTP requests, routing, and middleware integration.
+
+//3. Data Persistence: MongoDB, a NoSQL database used in MERN stack, stores data in a JSON-like format called BSON (Binary JSON). Since JavaScript inherently deals with JSON objects, it becomes easier to work with MongoDB using JavaScript.
+
+//4. Full-stack Development: JavaScript's ability to run on both the client-side (browser) and server-side (Node.js) makes it well-suited for full-stack development in the MERN stack. As developers only need to write code in one language throughout the entire application, it reduces the complexity and improves productivity.
+
+// Overall, JavaScript is an essential part of MERN development because it facilitates seamless communication between the front-end and back-end components of the application, allowing for efficient development and maintenance of web applications.
