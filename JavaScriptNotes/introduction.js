f89e81891032b21cdef64e0cbaeead7c39f4aac0
rajalaxmi_khatai <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-07-10 22:14:52 +0530

    adding some features of javascript

--- JavaScriptNotes/introduction.js
+++ JavaScriptNotes/introduction.js
@@ -3,3 +3,8 @@
 // One of the key features of JavaScript is its ability to be embedded directly into HTML documents, allowing developers to add interactivity to their websites. It is supported by all major web browsers, making it a versatile and accessible option for web development.
 
 // JavaScript is a multi-paradigm language, which means it supports both procedural and object-oriented programming styles. It provides a wide range of built-in functions and objects that can be used to manipulate web page elements, handle events, perform calculations, and interact with web APIs.
+// JavaScript is a high-level, interpreted programming language that is primarily used for creating interactive and dynamic web pages. It was initially developed by Brendan Eich at Netscape Communications and has since become one of the most widely used languages for client-side scripting on the web.
+
+// One of the key features of JavaScript is its ability to be embedded directly into HTML documents, allowing developers to add interactivity to their websites. It is supported by all major web browsers, making it a versatile and accessible option for web development.
+
+// JavaScript is a multi-paradigm language, which means it supports both procedural and object-oriented programming styles. It provides a wide range of built-in functions and objects that can be used to manipulate web page elements, handle events, perform calculations, and interact with web APIs.
