5b291177e143a38ed3754fe403291b82bca33c99
rajalaxmi_khatai <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-07-11 22:38:24 +0530

    updated the intro.js file

--- JS_Practice_SourceCode/intro.js
+++ JS_Practice_SourceCode/intro.js
@@ -1,5 +1,5 @@
 // console.log("welcome to javascript world");
 //writing the first code 
 //variables in javascript
-var myVariable = "Hello, World!";
+var myVariable = "Hello, World!";//var is a keywordand myvariable is variale name 
 console.log(myVariable);
