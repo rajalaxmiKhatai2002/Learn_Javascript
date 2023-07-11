3034ba01fb8d7ceb3318ea93ccd2e62eeecaea3c
rajalaxmi_khatai <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-07-11 22:37:33 +0530

    updated the intro.js file

--- JS_Practice_SourceCode/intro.js
+++ JS_Practice_SourceCode/intro.js
@@ -1,2 +1,5 @@
-console.log("welcome to javascript world");
+// console.log("welcome to javascript world");
 //writing the first code 
+//variables in javascript
+var myVariable = "Hello, World!";
+console.log(myVariable);
