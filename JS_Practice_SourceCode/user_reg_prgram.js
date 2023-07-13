129f92d03f60e234b0038d0e9af3300eac7eeac8
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-07-13 20:25:07 +0530

    file has been updataed

--- JS_Practice_SourceCode/user_reg_prgram.js
+++ JS_Practice_SourceCode/user_reg_prgram.js
@@ -2,9 +2,10 @@
 let username = "mern developer";
 let full_Nmae = "rajalaxmi khatai";
 const eMail = "rajalaxmi@2002.gmail.com";                                    //after decalring const variable type the value of that variable cant be chnaged.or tha value of that variable becaome constant.
-let Phone_Number = "746837256825792856";
+let Phone_Number = 746837256825792856;
 let password = "uyriuyr";
+let rollno = 34;
 
 
 
-console.log(typeof full_Nmae);      //to check the variable is what type
+console.log(typeof Phone_Number );      //to check the variable is what type
