7d6c420a72ac57fd192e1db0aeaa55541f03bcb2
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-07-13 20:45:36 +0530

    the program and also the output is correctly showed as we want

--- JS_Practice_SourceCode/user_reg_prgram.js
+++ JS_Practice_SourceCode/user_reg_prgram.js
@@ -8,4 +8,13 @@ let rollno = 34;
 
 
 
-console.log(typeof Phone_Number );      //to check the variable is what type
+// console.log(typeof Phone_Number ); //to check the variable is what type
+// console.log("my username is " + username + "my mobile name is" + Phone_Number );     //this + is used for concatenation of two vaues
+
+//we can also print the output like this is given bellow
+console.log(`
+    my username is : ${username},
+    my full name is : ${full_Nmae},
+    my email address is : ${eMail},
+    phone number is : ${Phone_Number}
+`);
