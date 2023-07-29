0cdff8938256ab9b12e76322d757a931899b9da2
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-07-29 20:40:49 +0530

    file has been updated

--- JS_Practice_SourceCode/function.js
+++ JS_Practice_SourceCode/function.js
@@ -172,10 +172,21 @@
   
 //   sayHi();
 
-for (var i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i), 1);
-  }
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(() => console.log(i), 1);
+//   }
   
-  for (let i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i), 1);
-  }
+//   for (let i = 0; i < 3; i++) {
+//     setTimeout(() => console.log(i), 1);
+//   }
+
+const shape = {
+    radius: 10,
+    diameter() {
+      return this.radius * 2;
+    },
+    perimeter: () => 2 * Math.PI * this.radius,
+  };
+  
+  console.log(shape.diameter());
+  console.log(shape.perimeter());
