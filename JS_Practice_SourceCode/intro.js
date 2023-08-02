8a1eb1b7765a1d84d005e7a90a3f8f8efa725bc9
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-08-02 21:11:20 +0530

    the file has been updated

--- JS_Practice_SourceCode/intro.js
+++ JS_Practice_SourceCode/intro.js
@@ -155,14 +155,14 @@
 // }
 //omit function
 
-let val1 = 10
-let val2 = 20
-function addnum(num1,num2){
-    let total = num1+num2
-    return total
-}
-let result1 = addnum(val1,val2)
-let result2 = addnum(45,76)
+// let val1 = 10
+// let val2 = 20
+// function addnum(num1,num2){
+//     let total = num1+num2
+//     return total
+// }
+// let result1 = addnum(val1,val2)
+// let result2 = addnum(45,76)
 
 
 
