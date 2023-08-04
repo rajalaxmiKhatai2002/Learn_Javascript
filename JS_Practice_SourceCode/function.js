cf50b8367415c59d44492a86387d1f76c03a896d
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-08-04 19:56:19 +0530

    file has been updated

--- JS_Practice_SourceCode/function.js
+++ JS_Practice_SourceCode/function.js
@@ -210,13 +210,27 @@
 // d = c;
 // c.greeting = 'Hello';
 // console.log(d.greeting);
-let a = 3;
-let b = new Number(3);
-let c = 3;
+// let a = 3;
+// let b = new Number(3);
+// let c = 3;
 
-console.log(a == b);
-console.log(a === b);
-console.log(b === c);
+// console.log(a == b);
+// console.log(a === b);
+// console.log(b === c);
+
+class Chameleon {
+    static colorChange(newColor) {
+      this.newColor = newColor;
+      return this.newColor;
+    }
+  
+    constructor({ newColor = 'green' } = {}) {
+      this.newColor = newColor;
+    }
+  }
+  
+  const freddie = new Chameleon({ newColor: 'purple' });
+  console.log(freddie.colorChange('orange'));
 
 
 
