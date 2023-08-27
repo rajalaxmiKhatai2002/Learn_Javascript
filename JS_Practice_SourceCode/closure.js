3811993a181ce898668a764dc56b44ade9e01254
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-08-27 22:26:39 +0530

    file has been updated

--- JS_Practice_SourceCode/closure.js
+++ JS_Practice_SourceCode/closure.js
@@ -1,10 +1,11 @@
 //lexical scope in javascript
-function outerfinction(){
-    let x = 5;
-    let y = 6;
-    function innerfunction(){
-        console.log(x);
-        console.log(y);
-    }
-    innerfunction();
-}
+// function outerfinction(){
+//     let x = 5;
+//     let y = 6;
+//     function innerfunction(){
+//         console.log(x);
+//         console.log(y);
+//     }
+//     innerfunction();
+// }
+
