33803b364a3309049cd2bc2760a30342b41d1dce
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-09-16 18:56:49 +0530

    the file has been updated

--- JS_Practice_SourceCode/intro.js
+++ JS_Practice_SourceCode/intro.js
@@ -236,9 +236,18 @@
 
 // const arr54 = [1, 2, 3, 4, 5,345,6456,4666666666,54,3];
 //  arr54.forEach((element,index) => console.log(element));
- const arr55 = [1, 2, 3, 4, 5];
-// arr55.forEach((element, index) => console.log(index));
-arr56.forEach((elem, index, ar) => co = ar[index] = elem * 2);
+//  const arr56 = [1, 2, 3, 4, 5];
+// // arr55.forEach((element, index) => console.log(index));
+// arr56.forEach((elem, index, ar) => co = ar[index] = elem * 2);
+// console.log(arr56);
+
+const arra2 = [24,53,45,75,44,56,465,46,47,453];
+ const atr = arra2.includes(1);
+console.log(atr);
+
+const arr57 = [1, 2, 3, 4, 5];
+ const result21 = arr57.includes(1);
+ console.log(result21); // true
 
 
 
