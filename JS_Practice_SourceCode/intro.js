3af679e3052e133f58b37127c24bb03b9697828a
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-09-16 18:57:30 +0530

    the file has been updated

--- JS_Practice_SourceCode/intro.js
+++ JS_Practice_SourceCode/intro.js
@@ -242,12 +242,12 @@
 // console.log(arr56);
 
 const arra2 = [24,53,45,75,44,56,465,46,47,453];
- const atr = arra2.includes(1);
+ const atr = arra2.includes(24);
 console.log(atr);
 
 const arr57 = [1, 2, 3, 4, 5];
  const result21 = arr57.includes(1);
- console.log(result21); // true
+ console.log(result21); 
 
 
 
