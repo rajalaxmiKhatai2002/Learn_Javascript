4a2837edd30b6e7bd224712802a2ba011c5273e8
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2023-09-18 21:50:00 +0530

    the file has been updated

--- JS_Practice_SourceCode/intro.js
+++ JS_Practice_SourceCode/intro.js
@@ -255,7 +255,9 @@
 const arr61 = [1, 2, 3, 4, 5];
 const result25 = Array.isArray(arr61);
 console.log(result25);
-
+const arr62 = [1, 2, 3, 4, 5];
+const result26 = Array.isArray(1);
+console.log(result26); 
 
 
 
